1e51b5b721cceb9e0293d8f03b28d826a495a99d
Muhammad Basim <basim@Basims-MacBook.local>
2026-03-31 03:30:51 +0500

    dev.js updated

--- web/dev.js
+++ web/dev.js
@@ -1,3 +1,2 @@
 // Dev wallet - loads the legacy full-featured dashboard
-const dev_wallet = require('flamingo-ui/src/node_modules/dev-wallet')
-dev_wallet()
+require('flamingo-ui/src/node_modules/dev-wallet')
